fb0aaad811795e0462704bdeb16c55a21854ed7c
Gabriel Zavando <gabrielzavando@gmail.com>
2019-09-13 23:07:35 -0300

    termine la maquetacion desktop

--- src/js/dev/slider.js
+++ src/js/dev/slider.js
@@ -1,8 +1,8 @@
 const carruselNormal = function sliderNormal(){
   const carrusel = document.querySelector('.Slider-content')
-  const carruselItem = document.querySelectorAll('.Slider-content img')
-  const prevBtn = document.getElementById('Prev')
-  const nextBtn = document.getElementById('Next')
+  const carruselItem = document.querySelectorAll('.Slider-content-item')
+  // const prevBtn = document.getElementById('Prev')
+  // const nextBtn = document.getElementById('Next')
 
   //Contador
   let contador = 1
@@ -10,20 +10,20 @@ const carruselNormal = function sliderNormal(){
 
   carrusel.style.transform = 'translateX(' + (-ancho * contador) + 'px)'
 
-  //Capturamos evento en botones
-  nextBtn.addEventListener('click', () => {
+  // Capturamos evento en botones
+  function siguiente(){
     if(contador >= carruselItem.length -1) return
     carrusel.style.transition = 'transform 0.5s ease-in-out'
     contador++
     carrusel.style.transform = 'translateX(' + (-ancho * contador) + 'px)'
-  })
+  }
 
-  prevBtn.addEventListener('click', () => {
-    if(contador <= 0) return
-    carrusel.style.transition = 'transform 0.5s ease-in-out'
-    contador--
-    carrusel.style.transform = 'translateX(' + (-ancho * contador) + 'px)'
-  })
+  // prevBtn.addEventListener('click', () => {
+  //   if(contador <= 0) return
+  //   carrusel.style.transition = 'transform 0.5s ease-in-out'
+  //   contador--
+  //   carrusel.style.transform = 'translateX(' + (-ancho * contador) + 'px)'
+  // })
 
   carrusel.addEventListener('transitionend', () => {
     if(carruselItem[contador].id === 'lastClone'){
@@ -37,6 +37,14 @@ const carruselNormal = function sliderNormal(){
       carrusel.style.transform = 'translateX(' + (-ancho * contador) + 'px)'
     }
   })
+
+  function playSlider(){
+    setInterval(siguiente, 2000)
+  }
+
+  playSlider()
 }
 
-export default carruselNormal
+export {
+  carruselNormal
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import {toggleNav, scrollNav, animaHeader, colorHeader, showButtonUp, upToTop} from './dev/nav'
 import {CopyDate} from './dev/footer'
 import {SliderFade} from './dev/slider-fade'
+// import {carruselNormal} from './dev/slider'
 
 
 toggleNav()
@@ -11,6 +12,7 @@ CopyDate()
 showButtonUp()
 upToTop()
 SliderFade()
+// carruselNormal()
 // printModal(bMForm, createModal,Form)
 // printModalClasic(bMOHistoria, mHistoria, bMCHistoria)
 // Mapa(mq768, mpC, mpG, mpImg)
